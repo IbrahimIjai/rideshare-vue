f52c5f419321dd0b7ded6df8aeeccb9df15b81f5
ibrahimijai <ibrahimsundayijai@gmail.com>
2023-12-05 12:51:32 +0100

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  </nav> -->
+  <!-- <router-view /> -->
+
+  <router-view v-slot="{ Component }">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style lang="scss">
